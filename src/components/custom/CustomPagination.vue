b14bf044e053d7303b5c531008afdc161ba83176
Vitor Santos <vitorjs82@gmail.com>
2025-01-24 09:16:29 -0300

    Paginação modularizada pra reutilização

---
+++ src/components/custom/CustomPagination.vue
@@ -0,0 +1,94 @@
+<script setup lang="ts">
+import {
+  Pagination,
+  PaginationEllipsis,
+  PaginationFirst,
+  PaginationLast,
+  PaginationList,
+  PaginationListItem,
+  PaginationNext,
+  PaginationPrev,
+} from "@/components/ui/pagination";
+import { Button } from "@/components/ui/button";
+import { reactive, toRefs, watch } from "vue";
+
+// Definição das props
+const props = defineProps({
+  pages: {
+    type: Object as () => {
+      current: number;
+      total: number;
+      last: number;
+    },
+    required: true,
+  },
+  selectPage: {
+    type: Function as () => (page: number) => void,
+    required: true,
+  },
+});
+
+// Estado reativo local para "p", sincronizado com as props
+const p = reactive({
+  current: props.pages.current,
+  total: props.pages.total,
+  last: props.pages.last,
+});
+
+// Atualiza "p" sempre que as props mudarem
+watch(
+    () => props.pages,
+    (newPages) => {
+      p.current = newPages.current;
+      p.total = newPages.total;
+      p.last = newPages.last;
+    },
+    { deep: true }
+);
+
+// Função para aplicar a lógica de filtro e alterar a página
+const applyFilter = (page: number) => {
+  if (props.selectPage) {
+    props.selectPage(page);
+  }
+};
+</script>
+
+<template>
+  <Pagination
+      class="w-full"
+      v-if="p.last > 1"
+      :total="p.total"
+      :items-per-page="10"
+      :sibling-count="1"
+      show-edges
+      :default-page="1"
+  >
+    <PaginationList v-slot="{ items }" class="flex items-center gap-2">
+      <PaginationFirst as-child @click="applyFilter(1)" />
+      <PaginationPrev as-child @click="applyFilter(p.current - 1)" />
+      <template v-for="(item, index) in items" :key="index">
+        <PaginationListItem
+            v-if="item.type === 'page'"
+            :value="item.value"
+            as-child
+        >
+          <Button
+              class="w-9 h-9 p-0"
+              :variant="item.value === p.current ? 'default' : 'outline'"
+              @click="applyFilter(item.value)"
+          >
+            {{ item.value }}
+          </Button>
+        </PaginationListItem>
+        <PaginationEllipsis v-else :index="index" />
+      </template>
+
+      <PaginationNext as-child @click="applyFilter(p.current + 1)" />
+      <PaginationLast as-child @click="applyFilter(p.last)" />
+    </PaginationList>
+  </Pagination>
+</template>
+
+<style scoped>
+</style>
