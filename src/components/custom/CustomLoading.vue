9c77eac5e22d53f0ae73a37821056bd0876b2fd2
Cleiton Teixeira <cleiton.teixeiras@gmail.com>
2025-03-15 23:20:04 -0300

    Update CustomLoading.vue

--- src/components/custom/CustomLoading.vue
+++ src/components/custom/CustomLoading.vue
@@ -1,28 +1,30 @@
 <template>
-  <div
-    class="items-center justify-center align-middle flex min-h-screen sm:w-full"
-  >
-    <div class="flex flex-col justify-center items-center flex-1">
-      <DotLottieVue
-        :style="{ height: '300px', width: '300px' }"
-        :src="loadingAnimation"
-        autoplay
-        loop
-      />
-      <div
-        :class="{
-          'opacity-0': !message.message,
-          'opacity-100': message.message,
-        }"
-        class="flex flex-col gap-3 transition-opacity duration-1000"
-      >
-        <div class="flex gap-1">
-          <Quote :size="20" :stroke-width="1.75" absoluteStrokeWidth />
-          <p class="max-w-[440px]">{{ message.message }}</p>
+  <div class="container">
+    <div
+      class="items-center justify-center align-middle flex min-h-screen sm:w-full"
+    >
+      <div class="flex flex-col justify-center items-center flex-1">
+        <DotLottieVue
+          :style="{ height: '300px', width: '300px' }"
+          :src="loadingAnimation"
+          autoplay
+          loop
+        />
+        <div
+          :class="{
+            'opacity-0': !message.message,
+            'opacity-100': message.message,
+          }"
+          class="flex flex-col gap-3 transition-opacity duration-1000"
+        >
+          <div class="flex gap-1">
+            <Quote :size="20" :stroke-width="1.75" absoluteStrokeWidth />
+            <p class="max-w-[440px]">{{ message.message }}</p>
+          </div>
+          <p class="text-left font-serif text-gray-700">
+            {{ message.signature }}
+          </p>
         </div>
-        <p class="text-left font-serif text-gray-700">
-          {{ message.signature }}
-        </p>
       </div>
     </div>
   </div>
