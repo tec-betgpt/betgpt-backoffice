ce02dcb59227a5bccbceb16cd9326b0fdcac555f
Vitor Santos <vitorjss82@gmail.com>
2025-03-18 03:11:59 -0300

    Add profile picture upload functionality
    Implemented functionality to add, replace, remove, and preview profile pictures in the user profile settings. Updated localization strings, improved layout styles, and configured TypeScript options to support JavaScript files. Additionally, added a new sheet UI for handling profile image updates with validation and error handling.

--- src/components/custom/CustomLoading.vue
+++ src/components/custom/CustomLoading.vue
@@ -1,8 +1,8 @@
 <template>
   <div
-    class="items-center justify-center align-middle flex min-h-screen sm:w-full"
+    class="items-center justify-center align-middle flex min-h-screen w-full"
   >
-    <div class="flex flex-col justify-center items-center flex-1">
+    <div class="flex flex-col justify-center items-center  w-full">
       <DotLottieVue
         :style="{ height: '300px', width: '300px' }"
         :src="loadingAnimation"
@@ -10,17 +10,19 @@
         loop
       />
       <div
-        :class="{
-          'opacity-0': !message.message,
-          'opacity-100': message.message,
-        }"
-        class="flex flex-col gap-3 transition-opacity duration-1000"
+          :class="{
+            'opacity-0': !message.message,
+            'opacity-100	': message.message,
+          }"
+          class="flex flex-col gap-2 sm:max-w-[440px] w-full  transition-opacity duration-1000"
       >
-        <div class="flex gap-1">
-          <Quote :size="20" :stroke-width="1.75" absoluteStrokeWidth />
-          <p class="max-w-[440px]">{{ message.message }}</p>
+        <div class=" flex px-8 sm:0 gap-1">
+          <Quote :size="20" :stroke-width="1.75" absoluteStrokeWidth  />
+          <p class="sm:text-lg text-sm font-sans  m-0 p-0">
+            {{ message.message }}
+          </p>
         </div>
-        <p class="text-left font-serif text-gray-700">
+        <p class="text-left font-serif text-xs sm:text-sm opacity-70 px-10">
           {{ message.signature }}
         </p>
       </div>
@@ -34,11 +36,11 @@ import { useConfigStore } from "@/stores/config";
 import { DotLottieVue } from "@lottiefiles/dotlottie-vue";
 import { Quote } from "lucide-vue-next";
 import { useColorMode } from "@vueuse/core";
-const mode = useColorMode();
 const configStore = useConfigStore();
 const message = computed(() => configStore.message);
+const colorMode = useColorMode();
 const loadingAnimation = computed(() =>
-  useColorMode().value === "dark"
+ colorMode.value === "dark"
     ? "/animation_logo_black.lottie"
     : "/animation_logo_white.lottie"
 );
--- src/views/configurations/Profile.vue
+++ src/views/configurations/Profile.vue
@@ -7,108 +7,182 @@
       </p>
     </div>
     <Separator class="mb-3" />
-    <div class="my-2">
-      <Label for="theme">Tema</Label>
-      <Select v-model="theme" id="theme">
-        <SelectTrigger class="col-span-3">
-          <SelectValue placeholder="Selecione um Tema" />
-        </SelectTrigger>
-        <SelectContent>
-          <SelectItem value="auto">Sistema</SelectItem>
-          <SelectItem value="dark">Preto</SelectItem>
-          <SelectItem value="light">Claro</SelectItem>
-        </SelectContent>
-      </Select>
-    </div>
-    <form @submit.prevent="submit" @keydown="form.onKeydown($event)">
-      <div class="mb-3">
-        <Label for="first_name">Primeiro nome</Label>
-        <Input
-          type="text"
-          id="first_name"
-          placeholder="Informe o seu primeiro nome"
-          v-model="form.first_name"
-          class="mt-1"
-        />
-        <HasError :form="form" field="first_name" />
-      </div>
-      <div class="mb-3">
-        <Label for="last_name">Sobrenome</Label>
-        <Input
-          type="text"
-          id="last_name"
-          placeholder="Informe o seu sobrenome"
-          v-model="form.last_name"
-          class="mt-1"
-        />
-        <HasError :form="form" field="last_name" />
-      </div>
-      <div class="mb-3">
-        <Label for="email">E-mail</Label>
-        <Input
-          type="text"
-          id="email"
-          placeholder="Informe o seu e-mail"
-          v-model="form.email"
-          class="mt-1"
-          :disabled="emailChangeRequest"
-        />
-        <HasError :form="form" field="email" />
-
-        <div
-          class="flex align-items-center text-sm font-medium mt-2"
-          v-if="emailChangeRequest"
-        >
-          Existe uma solicitação de troca de e-mail, você poderá cancelar a
-          solicitação clicando a seguir:
-          <Button
-            variant="outline"
-            @click="cancelEmailChange"
-            :disabled="loadingCancelEmailChange"
-            class="mt-2"
-          >
-            <LucideSpinner
-              v-if="loadingCancelEmailChange"
-              class="mr-2 h-4 w-4 animate-spin"
-            />
-            Cancelar Solicitação
-          </Button>
+    <div class=" flex justify-start gap-8 flex-wrap w-full">
+      <div>
+        <div class="my-2">
+          <Label for="theme">Tema</Label>
+          <Select v-model="theme" id="theme">
+            <SelectTrigger class="col-span-3">
+              <SelectValue placeholder="Selecione um Tema" />
+            </SelectTrigger>
+            <SelectContent>
+              <SelectItem value="auto">Sistema</SelectItem>
+              <SelectItem value="dark">Preto</SelectItem>
+              <SelectItem value="light">Claro</SelectItem>
+            </SelectContent>
+          </Select>
         </div>
-      </div>
-      <div class="mb-3">
-        <Label for="language_id">Idioma</Label>
-        <Select id="language_id" v-model="form.language_id">
-          <SelectTrigger class="col-span-3">
-            <SelectValue placeholder="Selecione um idioma" />
-          </SelectTrigger>
-          <SelectContent>
-            <SelectItem value="1">Português</SelectItem>
-          </SelectContent>
-        </Select>
-        <HasError :form="form" field="language_id" />
-      </div>
-      <div class="flex gap-2 justify-start">
-        <Button :disabled="loading" type="submit">
-          <LucideSpinner v-if="loading" class="mr-2 h-4 w-4 animate-spin" />
-          Atualizar perfil
-        </Button>
-      </div>
-    </form>
-  </div>
-</template>
+        <form @submit.prevent="submit" @keydown="form.onKeydown($event)">
 
+          <div class="mb-3">
+            <Label for="first_name">Primeiro nome</Label>
+            <Input
+                type="text"
+                id="first_name"
+                placeholder="Informe o seu primeiro nome"
+                v-model="form.first_name"
+                class="mt-1"
+            />
+            <HasError :form="form" field="first_name" />
+          </div>
+          <div class="mb-3">
+            <Label for="last_name">Sobrenome</Label>
+            <Input
+                type="text"
+                id="last_name"
+                placeholder="Informe o seu sobrenome"
+                v-model="form.last_name"
+                class="mt-1"
+            />
+            <HasError :form="form" field="last_name" />
+          </div>
+          <div class="mb-3">
+            <Label for="email">E-mail</Label>
+            <Input
+                type="text"
+                id="email"
+                placeholder="Informe o seu e-mail"
+                v-model="form.email"
+                class="mt-1"
+                :disabled="emailChangeRequest"
+            />
+            <HasError :form="form" field="email" />
+
+            <div
+                class="flex align-items-center text-sm font-medium mt-2"
+                v-if="emailChangeRequest"
+            >
+              Existe uma solicitação de troca de e-mail, você poderá cancelar a
+              solicitação clicando a seguir:
+              <Button
+                  variant="outline"
+                  @click="cancelEmailChange"
+                  :disabled="loadingCancelEmailChange"
+                  class="mt-2"
+              >
+                <LucideSpinner
+                    v-if="loadingCancelEmailChange"
+                    class="mr-2 h-4 w-4 animate-spin"
+                />
+                Cancelar Solicitação
+              </Button>
+            </div>
+          </div>
+          <div class="mb-3">
+            <Label for="language_id">Idioma</Label>
+            <Select id="language_id" v-model="form.language_id">
+              <SelectTrigger class="col-span-3">
+                <SelectValue placeholder="Selecione um idioma" />
+              </SelectTrigger>
+              <SelectContent>
+                <SelectItem value="1">Português</SelectItem>
+              </SelectContent>
+            </Select>
+            <HasError :form="form" field="language_id" />
+          </div>
+          <div class="flex gap-2 justify-start">
+            <Button :disabled="loading" type="submit">
+              <LucideSpinner v-if="loading" class="mr-2 h-4 w-4 animate-spin" />
+              Atualizar perfil
+            </Button>
+          </div>
+        </form>
+      </div>
+      <div  class="flex-1">
+        <div class="my-2 flex flex-col items-center justify-center gap-2 ">
+          <div class="mb-4 flex items-center justify-center gap-2">
+            <p class="text-lg font-medium">
+              Foto de Perfil
+            </p>
+            <Pencil :size="20" :stroke-width="1.55" @click="openSheet" class="cursor-pointer" />
+          </div>
+          <div class="mb-4 flex flex-col items-center justify-center gap-2">
+            <Avatar size="lg">
+              <AvatarImage v-if="authStore.user.media" :src="authStore.user?.media[0].original_url" />
+              <AvatarFallback>
+                {{ authStore.user?.initials }}
+              </AvatarFallback>
+            </Avatar>
+            <div>
+              <p>
+                {{authStore.user.name}}
+              </p>
+            </div>
+          </div>
+
+        </div>
+
+      </div>
+    </div>
+
+  </div>
+
+  <Sheet v-model:open="imageSheet" position="right" size="lg">
+
+    <SheetContent class="flex flex-col gap-4">
+      <SheetHeader>
+        <SheetTitle>
+          Atualizar Imagem de Perfil
+        </SheetTitle>
+      </SheetHeader>
+      <form @submit.prevent="updateImage" class="space-y-4">
+        <div>
+          <Label for="profileImage">Imagem de Perfil</Label>
+          <Input
+              type="file"
+              id="profileImage"
+              ref="fileInput"
+
+              @change="handleFileChange"
+              accept="image/*"
+              class="mt-2"
+          />
+        </div>
+        <SheetFooter>
+          <Button :disabled="loading || !selectedImagePreview"  type="submit">
+            <LucideSpinner v-if="loading" class="mr-2 h-4 w-4 animate-spin"/>
+            {{ hasImage ? "Atualizar Imagem" : "Adicionar Imagem" }}
+          </Button>
+        </SheetFooter>
+      </form>
+      <div v-if="selectedImagePreview" class="mt-4 text-sm">
+        <p>Pré-visualização:</p>
+        <img
+            :src="selectedImagePreview"
+            alt="Preview"
+            class="w-full h-auto mt-2 border rounded"
+        />
+      </div>
+      <div v-if="error" class="text-red-500 mt-2">
+        {{ error }}
+      </div>
+    </SheetContent>
+
+  </Sheet>
+
+
+</template>
 <script setup lang="ts">
-import { ref, onMounted, watch } from "vue";
+import {ref, onMounted, watch, computed} from "vue";
 import { useToast } from "@/components/ui/toast/use-toast";
 import { useAuthStore } from "@/stores/auth";
 import api from "@/services/api";
 import Form from "vform";
 Form.axios = api;
 
-import { Loader2 as LucideSpinner } from "lucide-vue-next";
+import {Loader2 as LucideSpinner, Pencil} from "lucide-vue-next";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
-import { Checkbox } from "@/components/ui/checkbox";
 import { Label } from "@/components/ui/label";
 import {
   Select,
@@ -119,6 +193,8 @@ import {
 } from "@/components/ui/select";
 import { Separator } from "@/components/ui/separator";
 import { useColorMode } from "@vueuse/core";
+import {Avatar, AvatarFallback, AvatarImage} from "@/components/ui/avatar";
+import {Sheet, SheetContent, SheetFooter, SheetHeader, SheetTitle} from "@/components/ui/sheet";
 const theme = ref(localStorage.getItem("theme") || "auto");
 const { toast } = useToast();
 const authStore = useAuthStore();
@@ -126,15 +202,16 @@ const loading = ref(false);
 const loadingCancelEmailChange = ref(false);
 const emailChangeRequest = ref(false);
 
-const mode = useColorMode();
 const form = ref(
   new Form({
     first_name: "",
     last_name: "",
     email: "",
     language_id: "",
+    image: "",
   })
 );
+
 
 watch(theme, () => {
   setTheme();
@@ -172,7 +249,6 @@ const submit = async () => {
     loading.value = false;
   }
 };
-
 const fetchEmailChangeRequest = async () => {
   try {
     const response = await api.get("/user/configurations/email-change-request");
@@ -184,7 +260,6 @@ const fetchEmailChangeRequest = async () => {
     emailChangeRequest.value = false;
   }
 };
-
 const cancelEmailChange = async () => {
   loadingCancelEmailChange.value = true;
   try {
@@ -203,7 +278,6 @@ const cancelEmailChange = async () => {
     loadingCancelEmailChange.value = false;
   }
 };
-
 onMounted(fetchEmailChangeRequest);
 
 watch(
@@ -223,4 +297,82 @@ watch(
   },
   { immediate: true }
 );
+
+const openSheet = ()=>{
+  form.value.image = "";
+  selectedImagePreview.value = null;
+  imageSheet.value = true;
+}
+
+const imageSheet = ref(false);
+const fileInput = ref();
+const selectedImagePreview = ref<string | null>(null);
+const error = ref();
+const hasImage = computed(() => authStore.user?.media ? !!authStore.user.media[0]?.original_url : false);
+const handleFileChange = (event) => {
+  const file = event.target.files[0];
+
+  if (!file) return;
+  if (file.size > 4196 * 1024) {
+    error.value = "A imagem deve ter no máximo 4MB.";
+    return;
+  }
+
+  const img = new Image();
+  img.src = URL.createObjectURL(file);
+
+  img.onload = () => {
+
+    if (img.width > 4096 || img.height > 2160) {
+      error.value = "A imagem deve ter no máximo 4096 x 2160 pixels.";
+      selectedImagePreview.value = "";
+      form.value.image = "";
+      URL.revokeObjectURL(img.src);
+      return;
+    }
+    error.value = "";
+    form.value.image = file;
+    selectedImagePreview.value = img.src;
+  };
+
+  img.onerror = () => {
+    error.value = "Erro ao carregar a imagem.";
+  };
+};
+
+const updateImage = async () => {
+  if (!form.value.image) {
+    error.value = "Nenhum arquivo selecionado.";
+    console.log(form.value)
+    return;
+  }
+
+  loading.value = true;
+  error.value = null;
+
+  try {
+    await form.value.post("/user/configurations/update-photo", {
+      headers: {
+        "Content-Type": "multipart/form-data",
+      },
+    });
+    toast({
+      title: "Sucesso",
+      description: hasImage
+          ? "Imagem atualizada com sucesso."
+          : "Imagem adicionada com sucesso.",
+    });
+
+    // Refresh user data or update authStore.avatar...
+    authStore.fetchUser();
+    selectedImagePreview.value = null;
+    imageSheet.value = false;
+  } catch (err) {
+    error.value = "Falha ao atualizar a imagem.";
+    console.error(err);
+  } finally {
+    loading.value = false;
+  }
+};
+
 </script>
