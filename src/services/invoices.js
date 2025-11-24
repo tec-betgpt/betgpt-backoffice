7c1124377b66378c84aa559a4d3c8dbe49214436
Tavares <gerson.c.tavares@gmail.com>
2025-11-24 17:52:35 -0300

    Feat: [WEB] Adicionar novo componente para buscar link de pagamento na seção de faturas

--- src/services/invoices.js
+++ src/services/invoices.js
@@ -23,5 +23,13 @@ export default {
   async byHash(hash) {
     const { data } = await api.get(`/invoices/by-hash/${hash}`)
     return data
+  },
+
+  /**
+   * GET /v1/invoices/get-link/:id
+   */
+  async getLink(id) {
+    const { data } = await api.get(`/invoices/get-link/${id}`)
+    return data
   }
 }
