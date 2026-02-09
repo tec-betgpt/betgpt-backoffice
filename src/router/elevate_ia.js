174ed5f668dd3e7899f44f16847de6e5d01d9945
Vitor Santos <vitorjss82@gmail.com>
2026-02-09 09:57:21 -0300

    add audio recording

--- src/router/elevate_ia.js
+++ src/router/elevate_ia.js
@@ -11,7 +11,7 @@ export default [
       requiresAuth: true,
       roles: "member|client",
       permissions: "access-to-ai",
-      title: "Elevate IA",
+      title: "Elevate Brain",
     },
     children: [
       {
