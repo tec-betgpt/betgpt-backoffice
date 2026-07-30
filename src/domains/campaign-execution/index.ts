07be91719445d2fdd5e1638246e99b686627d04f
Tavares <gerson.c.tavares@gmail.com>
2026-07-30 11:37:33 -0300

    Refactor: ajuste em alocação de arquivos

--- src/domains/campaign-execution/index.ts
+++
@@ -1,8 +0,0 @@
-export { default as campaignExecutionApi } from "./api";
-export * from "./api";
-
-export {
-  extractExecutionErrorMessage,
-  useCampaignExecutionStore,
-} from "./store";
-export type { CampaignExecutionAction } from "./store";
